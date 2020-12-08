81f28827ce7857636892de1a2da909b2a2533a4c
somi <nnn0111123@naver.com>
2020-12-08 15:16:37 +0900

    connect firebase

--- app/src/main/java/com/example/howlstagram/LoginActivity.kt
+++ app/src/main/java/com/example/howlstagram/LoginActivity.kt
@@ -1,11 +1,57 @@
 package com.example.howlstagram
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
+
+    var auth : FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        email_login_button.setOnClickListener{
+            signinAndSignup()
+        }
+    }
+    fun signinAndSignup(){
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            ?.addOnCompleteListener{ //회원가입한 결과값 가져옴
+            task ->
+                if(task.isSuccessful){
+                    //아이디가 생성되었을 때(회원가입)
+                    moveMainPage(task.result?.user)
+                } else if(task.exception?.message.isNullOrEmpty()) {
+                    //Show the error message
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }else{
+                    //로그인
+                }
+        }
+    }
+    fun signinEmail(){ //로그인
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            ?.addOnCompleteListener{ //회원가입한 결과값 가져옴
+                    task ->
+                if(task.isSuccessful){
+                    //Login
+                    moveMainPage(task.result?.user)
+                } else{
+                    //Show the error message
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    fun moveMainPage(user: FirebaseUser?){ //파이어베이스 유저상태
+        if(user != null){ //유저상태 존재
+            startActivity(Intent( this,MainActivity::class.java)) //메인페이지로
+        }
     }
 }
